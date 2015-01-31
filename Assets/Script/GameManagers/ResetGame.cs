5c0005bf4c9598f378b85325650ace6921debe8f
nikhil <potenikhil@gmail.com>
2015-01-31 14:08:13 +0530

    Animation and collision bug removed

---
+++ Assets/Script/GameManagers/ResetGame.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetGame {
+	public ResetGame() {
+		ScoreManager.resetScore ();
+	}
+
+}
---
+++ Assets/Script/GameManagers/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager  {
+
+	// Use this for initialization
+	private static int score;
+
+	public static void increaseScore() {
+		score += 1;
+	}
+	public static void increaseScoreBy(int _score) {
+		score += _score;
+	}
+	public static void resetScore() {
+		score = 0;
+	}
+
+	public static int getScore() {
+		return score;
+	}
+}
